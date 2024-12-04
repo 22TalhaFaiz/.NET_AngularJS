8a92d60ec111006b68a6b2a0c4f4a92f3fa65cd3
22TalhaFaiz <138751622+22TalhaFaiz@users.noreply.github.com>
2024-12-04 18:53:24 +0500

    Work ANGJS + NET

---
+++ CRUD AngularJS_.NET/ATest/WebApplication1/Models/Connection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+	public class Connection : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseSqlServer("server=.; database=Angulartest; User Id=sa; password=aptech; TrustServerCertificate=True");
+		}
+		public DbSet<User> users { get; set; }
+	}
+}
